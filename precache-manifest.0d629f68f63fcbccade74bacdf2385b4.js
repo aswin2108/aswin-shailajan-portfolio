6fa909a69e36d42a99462f16f6265a3374cb4aae
aswins2108@gmail.com <aswins2108@gmail.com>
2023-06-16 11:31:06 +0530

    Updates

---
+++ precache-manifest.0d629f68f63fcbccade74bacdf2385b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3fa002fe446290062d1",
+    "url": "/aswin-shailajan-portfolio/static/css/main.918deeb8.chunk.css"
+  },
+  {
+    "revision": "d3fa002fe446290062d1",
+    "url": "/aswin-shailajan-portfolio/static/js/main.863dc5c2.chunk.js"
+  },
+  {
+    "revision": "874419589f9410df89de",
+    "url": "/aswin-shailajan-portfolio/static/js/runtime~main.83c79a21.js"
+  },
+  {
+    "revision": "928713cb9fc3a40fb8e3",
+    "url": "/aswin-shailajan-portfolio/static/js/2.05ef40bd.chunk.js"
+  },
+  {
+    "revision": "f4866491ae15624a43d3",
+    "url": "/aswin-shailajan-portfolio/static/js/3.b700955a.chunk.js"
+  },
+  {
+    "revision": "842e320f0e8473e2d7c3c056b00df030",
+    "url": "/aswin-shailajan-portfolio/static/media/logo-image.842e320f.png"
+  },
+  {
+    "revision": "26df447c41afd7bcffcf4c677e4bf56f",
+    "url": "/aswin-shailajan-portfolio/static/media/Hi.26df447c.gif"
+  },
+  {
+    "revision": "e7df442f8091596650b9aad6bd5f25ea",
+    "url": "/aswin-shailajan-portfolio/index.html"
+  }
+];
